5f95f78cfda290329bc4bbdb69967b75cadd6a1d
Євгеній <321linkbuild123@email.com>
2026-04-01 12:43:56 +0300

    RxJs Intro

---
+++ src/app/courses-page/courses-page.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule, FormControl } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { debounceTime, distinctUntilChanged, switchMap, startWith } from 'rxjs/operators';
+import { CourseService, Course } from './course';
+
+@Component({
+  selector: 'app-courses-page',
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule],
+  templateUrl: './courses-page.html',
+  styleUrls: ['./courses-page.css']
+})
+export class CoursesPage implements OnInit {
+  searchControl = new FormControl('');
+  courses$!: Observable<Course[]>;
+
+  constructor(private courseService: CourseService) { }
+
+  ngOnInit(): void {
+    this.courses$ = this.searchControl.valueChanges.pipe(
+      startWith(''),
+      debounceTime(400),
+      distinctUntilChanged(),
+      switchMap(query => this.courseService.searchCourses(query || ''))
+    );
+  }
+}
